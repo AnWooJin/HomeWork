c5715b6c15612280dd80bf86aa25154deac99cf4
asus <asus@LAPTOP-9CPJDSIR>
2022-01-14 15:51:43 +0900

    안우진: HomeWork220112 커밋

---
+++ HomeWork220112/HomeWork220112.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+	{
+		int Arr[10] = { 0, };
+		int Index = 0;
+		while (Index < 10)
+		{
+			// 0 부터 시작해서
+			Arr[Index] = 2 + (Index * 2);
+			Index += 1;
+			// 0 제외
+			// 2 4 8 10 12 14 16 18 
+			// 배열이 가득찰때까지 짝수로만 채워주세요
+		}
+	}
+
+	{
+		int Arr[10] = { 0, };
+		int Index = 0;
+		while (Index < 10)
+		{
+			// 0 부터 시작해서
+			Arr[Index] = 1 + (Index * 2);
+			Index += 1;
+			// 0 제외
+			// 2 4 8 10 12 14 16 18 
+			// 배열이 가득찰때까지 홀수로만 채워주세요
+		}
+
+		int a = 0;
+	}
+}
