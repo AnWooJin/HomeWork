d10153e0be5fbf00cd0d05e06b833d7b9059e405
asus <asus@LAPTOP-9CPJDSIR>
2022-01-17 17:08:58 +0900

    안우진: HomeWork220117 2개 커밋

---
+++ HomeWork220115/HomeWork220117CountFirst.cpp
@@ -0,0 +1,63 @@
+﻿// HomeWork220115.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+int StringCount(const char* _Text) {
+    int Count = 0;
+    while (0 != _Text[Count])
+    {
+        Count += 1;
+    }
+
+    return Count;
+}
+
+int CountFirst(const char* _Text, int _Start, const char* _FindStr)
+{
+    int TextCount = StringCount(_Text);
+    int FindCount = StringCount(_FindStr);
+    int Find_idx = _Start;
+    bool check = false;
+    if (_Start < 0 || _Start > TextCount)
+    {
+        return -1;
+    }
+    while (Find_idx + (FindCount - 1) < TextCount)
+    {
+        for (; _Text[Find_idx] != _FindStr[0] && Find_idx < TextCount; Find_idx += 1);
+        if (_Text[Find_idx] == _FindStr[0])
+        {
+            for (int i = 0; i < FindCount; i += 1)
+            {
+                if (_Text[Find_idx + i] == _FindStr[i])
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                    break;
+                }
+            }
+        }
+        if (check == true)
+        {
+            return Find_idx;
+        }
+        Find_idx += 1;
+    }
+    return -1;
+}
+
+int CountLast(const char* _Text, int _End, const char* _FindStr)
+{
+    return -1;
+}
+
+
+int main()
+{
+    // 실패
+    int Count = CountFirst("aaa eee ttt asdfasd eee", 4, "kee");
+  
+}
---
+++ HomeWork220117CountLast/HomeWork220117CountLast.cpp
@@ -0,0 +1,63 @@
+﻿
+#include <iostream>
+
+int StringCount(const char* _Text) {
+    int Count = 0;
+    while (0 != _Text[Count])
+    {
+        Count += 1;
+    }
+
+    return Count;
+}
+
+
+
+int CountLast(const char* _Text, int _End, const char* _FindStr)
+{
+    int TextCount = StringCount(_Text);
+    int FindCount = StringCount(_FindStr);
+    int Find_idx = _End;
+    bool check = false;
+    if (_End > TextCount || _End < 0)
+    {
+        return -1;
+    }
+    while (Find_idx - (FindCount - 1) > 0)
+    {
+        for (; _Text[Find_idx] != _FindStr[0] && Find_idx > 0; Find_idx -= 1);
+        if (_Text[Find_idx] == _FindStr[0])
+        {
+            for (int i = 0; i < FindCount; i += 1)
+            {
+                if (_Text[Find_idx - i] == _FindStr[i])
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                    break;
+                }
+            }
+        }
+        if (check == true)
+        {
+            return Find_idx;
+        }
+        Find_idx -= 1;
+    }
+    return -1;
+}
+
+
+
+int main()
+{
+
+    int Count = CountLast("aaa eae asdfasd eee", 15, "eee");
+    int a = 10;
+
+
+}
+
